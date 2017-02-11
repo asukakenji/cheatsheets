772f238b1dab471885ff3310c629eab5088ec16a
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2017-02-11 17:58:07 +0800

    Updated 'c-dynamiclib/' and 'java-to-c/'

--- c-dynamiclib/clean.sh
+++ c-dynamiclib/clean.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-rm -f Main.class
-rm -f Main.h
 rm -f libdemo.dylib
--- c-dynamiclib/make.sh
+++ c-dynamiclib/make.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-# Linux
-clang -shared -o libdemo.so libdemo.c
+case "$OSTYPE" in
+darwin*)
+    # Mac (New Way)
+    clang -shared -o libdemo.dylib libdemo.c
 
-# Mac (New Way)
-clang -shared -o libdemo.A.dylib libdemo.c
-
-# Mac (Traditional Way)
-# Reference:
-# https://developer.apple.com/library/content/documentation/DeveloperTools/Conceptual/DynamicLibraries/100-Articles/CreatingDynamicLibraries.html
-clang -dynamiclib -current_version 1.0 -compatibility_version 1.0 -o libdemo.A.dylib libdemo.c
+    # Mac (Traditional Way)
+    # Reference:
+    # https://developer.apple.com/library/content/documentation/DeveloperTools/Conceptual/DynamicLibraries/100-Articles/CreatingDynamicLibraries.html
+    #clang -dynamiclib -current_version 1.0 -compatibility_version 1.0 -o libdemo.A.dylib libdemo.c
+    ;;
+linux*)
+    # Linux
+    clang -shared -o libdemo.so libdemo.c
+    ;;
+*)
+    echo "Unknow OS"
+    exit 1
+esac
