79201e3690a8f936d44f5f318d956f8dcccc3997
Siu Ching Pong -Asuka Kenji- <asukakenji@users.noreply.github.com>
2017-02-12 19:11:24 +0800

    Minor amendments

--- java-to-c/Main.c
+++ java-to-c/Main.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 
+/* The signature should be the same as that generated in Main.h */
 JNIEXPORT void JNICALL Java_Main_printOkay(JNIEnv* env, jclass clazz)
 {
     printf("Okay\n");
